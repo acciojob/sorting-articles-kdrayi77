1cacf22fde15d01f3f2036bd4153e8f02ea92d61
kdrayi77 <120662678+kdrayi77@users.noreply.github.com>
2025-07-28 09:23:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
- // const bands = [
- //      'The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil',
- //      'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State',
- //      'We Came as Romans', 'Counterparts', 'Oh, Sleeper',
- //      'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'
- //    ];
+ const bands = [
+      'The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil',
+      'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State',
+      'We Came as Romans', 'Counterparts', 'Oh, Sleeper',
+      'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'
+    ];
 
- //    function stripArticles(name) {
- //      return name.replace(/^(a |an |the )/i, '').trim();
- //    }
+    function stripArticles(name) {
+      return name.replace(/^(a |an |the )/i, '').trim();
+    }
 
- //    const sortedBands = bands.sort((a, b) => {
- //      return stripArticles(a).localeCompare(stripArticles(b));
- //    });
+    const sortedBands = bands.sort((a, b) => {
+      return stripArticles(a).localeCompare(stripArticles(b));
+    });
 
- //     console.log(sortedBands.join('\n'));
+     console.log(sortedBands.join('\n'));
  
